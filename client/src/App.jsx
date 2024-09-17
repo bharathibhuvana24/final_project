74b0dd3e1c5b787b841f44859552000d4a80cf27
bharathibhuvana24 <bharathibhuvana24@gmail.com>
2024-09-17 21:42:30 +0530

    created pages and routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
 import React from 'react'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SignOut from './Pages/SignOut'
+import Profile from './Pages/Profile'
+
 
 const App = () => {
   return (
-    <div>App</div>
+<>
+<BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/sign-in' element={<SignIn/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+    <Route path='/sign-out' element={<SignOut/>}/>
+  </Routes>
+</BrowserRouter>
+</>
+   
   )
 }
 
