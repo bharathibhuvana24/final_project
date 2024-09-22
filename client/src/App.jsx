66f8f8824df3b5c6882b36f37651d12d7f423ff0
bharathibhuvana24 <bharathibhuvana24@gmail.com>
2024-09-22 11:28:46 +0530

    created router and paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,22 @@ import React from 'react'
 import Home from './Pages/Home'
 import About from './Pages/About'
 import SignIn from './Pages/SignIn'
-import SignOut from './Pages/SignOut'
 import Profile from './Pages/Profile'
+import Header from './Components/Header'
+import SignUp from './Pages/SignUp'
 
 
 const App = () => {
   return (
 <>
 <BrowserRouter>
+<Header/>
   <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/about' element={<About/>}/>
     <Route path='/sign-in' element={<SignIn/>}/>
     <Route path='/profile' element={<Profile/>}/>
-    <Route path='/sign-out' element={<SignOut/>}/>
+    <Route path='/signup' element={<SignUp/>}/>
   </Routes>
 </BrowserRouter>
 </>
