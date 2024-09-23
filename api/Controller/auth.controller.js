f215f7a6493ae7089230cb1ec587c67ef6bcf360
bharathibhuvana24 <bharathibhuvana24@gmail.com>
2024-09-23 19:28:33 +0530

    created a middleware and a function to handle possible errors

--- api/Controller/auth.controller.js
+++ api/Controller/auth.controller.js
@@ -11,6 +11,6 @@ export const signup = async(req,res) =>
         await newUser.save();
         res.status(201).json({message:"User created successfully"});
     }catch(error){
-        res.status(500).json({message:"Request failed"});
+        next(error);
     }
 }
--- api/index.js
+++ api/index.js
@@ -24,6 +24,16 @@ mongoose.connect(mongodb_connection_string).then(() =>
 
     app.use("/api/user",userRouter);
     app.use("/api/auth", authRouter);
+    app.use((err,req,res,next)=>
+    { 
+        const statusCode = err.statusCode || 500;
+        const message = err.message || "Internal Server Error";
+        return res.status(statusCode).json({
+            success:false,
+            message,
+            statusCode
+        })
+    })
 
 
 app.listen(3000,()=>
