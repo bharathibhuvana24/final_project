c68fefbe3f6b1b793e7db753af6589423eaca859
bharathibhuvana24 <bharathibhuvana24@gmail.com>
2024-09-23 11:02:57 +0530

    changes made

--- api/index.js
+++ api/index.js
@@ -1,8 +1,25 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
 
 const app = express();
 
+mongoose.connect(process.env.MONGO).then(() =>
+    {
+        console.log("connected to MongoDB")
+    }).catch((err) => 
+        {console.log("Error:" +err)});
+    
+
+
+    app.get('/',(req,res)=>
+{
+    res.status(200).send("App is working")
+    
+})
+
 app.listen(3000,()=>
 {
-    console.log('server is running in the port 3000');
-})
+    console.log("server is running in the port 3000");
+});
