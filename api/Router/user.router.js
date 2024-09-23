7cad150f35df807f0e62b566520866d57bab46b4
bharathibhuvana24 <bharathibhuvana24@gmail.com>
2024-09-23 11:54:00 +0530

    created simple user model, controller and router

---
+++ api/Router/user.router.js
@@ -0,0 +1,5 @@
+import express from 'express'; 
+import { test } from '../Controller/user.controller.js';
+const router = express.Router();
+router.get("/test", test)
+export default router;
--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from "express"
 import mongoose from "mongoose";
+import userRouter from "./Router/user.router.js";
 import dotenv from "dotenv";
 dotenv.config()
 
 const app = express();
+const mongodb_connection_string = process.env.MONGO;
 
-mongoose.connect(process.env.MONGO).then(() =>
+mongoose.connect(mongodb_connection_string).then(() =>
     {
         console.log("connected to MongoDB")
     }).catch((err) => 
@@ -15,9 +17,10 @@ mongoose.connect(process.env.MONGO).then(() =>
 
     app.get('/',(req,res)=>
 {
-    res.status(200).send("App is working")
-    
+    res.status(200).send("App is working!!!");
 })
+
+    app.use("/api/user",userRouter);
 
 app.listen(3000,()=>
 {
