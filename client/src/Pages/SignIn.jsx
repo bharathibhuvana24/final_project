4a87b16d6951b65491212adc4de4ba54c26734af
bharathibhuvana24 <bharathibhuvana24@gmail.com>
2024-09-24 15:05:11 +0530

    added redux toolkit

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,29 +1,82 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+
+import { useNavigate } from 'react-router-dom';
+ import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import { signInStart, signInFailure, signInSuccess } from '../redux/user/userSlice';
+
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+ 
+  const handleChange=(e)=>{
+    setFormData(
+      {
+        ...formData,
+        [e.target.id]:e.target.value
+      })
+  };
+
+  const handleSubmit=  async (e)=>{
+    e.preventDefault();
+    try {
+      dispatch(signInStart());
+    const res = await fetch('/api/auth/signin',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData),
+    });
+    const text = await res.text(); // Get the raw response text
+  console.log(text); // Log the raw response
+  
+    const data = JSON.parse(text); // Parse the response text as JSON
+    console.log(data);
+
+    if(data.success === false){
+      dispatch(signInFailure(data.message));
+      return;
+    }
+    dispatch(signInSuccess(data));
+    navigate('/');
+
+  } catch (error) {
+    dispatch(signInFailure(error.message));
+    
+  }
+};
+    
+  
+  console.log(formData);
    
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="email"
           placeholder="Email"
           className="border p-3 rounded-lg"
           id="email"
-          
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Password"
           className="border p-3 rounded-lg"
           id="password"
-                 
+          onChange={handleChange}
         />
-        <button  className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-        Sign-In
-        </button>
+        
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+        {loading ? 'Loading...' : 'Sign In'}
+      </button>
+ 
 
       </form>
 
@@ -33,7 +86,7 @@ const SignIn = () => {
         <span className="text-blue-700">Sign Up</span>
         </Link>
       </div>
-    
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
